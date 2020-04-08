1fb071bc603f848e3e3ff4df0a926bce17817c8b
Henry Calderon <theceintiant@gmail.com>
2020-04-08 11:55:24 -0700

    Second Commit for Problem 1.

--- s_c_problem1.py
+++ s_c_problem1.py
@@ -23,16 +23,28 @@ Function of find duplicates( takes in array)
 
 '''
 list1 = [1,2,3,4,5,5,6,7,8,9,]
+list2 = [9,4,2,3,4,8,10,11]
+list3 = [2,3,4,5,6,7,8,9,0]
 
 def find_dup(array):
-        dup_array = []
-        for number in array:
-            if number in array:
-                return True
-            else:
-                dup_array.append(number)
-        #Returning false if no dup is found
-        return False
+    '''
+    This turned out incorrect as it always returned true because it would
+    just check that the number was in the array
+    Should be (as is now fixed), that it checks if it in the dupe array
+    '''
+    
+    # This is a brute force method with time complexity of 0(n^2)
+    dupe_array = []
+    for number in array:
+        if number in dupe_array:
+            return True
+        else:
+            dupe_array.append(number)
+    #Returning false if no dup is found
+    return False
+
 
 
 print(find_dup(list1))
+print(find_dup(list2))
+print(find_dup(list3))
